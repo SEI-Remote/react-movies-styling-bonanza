1be700f788bc736993c818652374db8629cdb697
Ben Manley <benjamintmanley@gmail.com>
2024-05-02 22:36:04 -0500

    implement theme selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-
+import { useState, useEffect } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import Landing from './pages/Landing/Landing'
 import MovieList from './pages/MovieList/MovieList'
@@ -9,14 +9,32 @@ import './App.css'
 
 function App() {
 
+  const [currentTheme, setCurrentTheme] = useState('theme-a')
+
+  useEffect(() => {
+    let theme = localStorage.getItem('theme')
+    if (!theme) {
+      handleChangeTheme('theme-a')
+    } else {
+      handleChangeTheme(theme)
+    }
+  }, [])
+
+  const handleChangeTheme = newTheme => {
+    setCurrentTheme(newTheme)
+    localStorage.setItem('theme', newTheme)
+  }
+
   return (
     <>
-      <NavBar />
+    <div className={currentTheme}>
+      <NavBar currentTheme={currentTheme} handleChangeTheme={handleChangeTheme} />
       <Routes>
         <Route path='/' element={<Landing />}/>
         <Route path='/movies' element={<MovieList />} />
         <Route path='/movies/:movieId' element={<MovieDetails />} />
       </Routes>
+    </div>
     </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,28 @@ import { NavLink } from 'react-router-dom'
 import navIcon from '../../assets/icon1.png'
 import './NavBar.css'
 
-const NavBar = () => {
+const NavBar = (props) => {
+
+  const handleSelectTheme = evt => {
+    props.handleChangeTheme(evt.target.value)
+  }
+
   return (
     <nav>
       <NavLink to='/'><img className='logo' src={navIcon} alt="a movie film camera" /></NavLink>
+      <select name="theme" onChange={handleSelectTheme}>
+        <option value={props.currentTheme}>{props.currentTheme[0].toUpperCase() + 'heme ' + props.currentTheme[6].toUpperCase()}</option>
+        <option value="theme-a">Theme A</option>
+        <option value="theme-b">Theme B</option>
+        <option value="theme-c">Theme C</option>
+        <option value="theme-d">Theme D</option>
+        <option value="theme-e">Theme E</option>
+        <option value="theme-f">Theme F</option>
+        <option value="theme-g">Theme G</option>
+        <option value="theme-h">Theme H</option>
+        <option value="theme-i">Theme I</option>
+        <option value="theme-j">Theme J</option>
+      </select>
       <NavLink to='/movies'>Movies</NavLink>
     </nav>
   )
