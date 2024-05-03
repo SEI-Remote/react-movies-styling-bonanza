e51bf26a34d765fa8f888649958411202d52c07f
ManliestBen <benjamintmanley@gmail.com>
2024-05-03 01:02:59 -0500

    add styling modes and ability to visualize theme colors within MovieDetails component

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -10,6 +10,21 @@ const MovieDetails = () => {
   return (
     <>
       <h1>{movie.title}</h1>
+      <div className='test-sq' style={{height: '500px', width: '500px', background: 'var(--bg-primary)'}}>
+        ←←←←←←  bg-primary →→→→→→
+        <div className='test-sq' style={{height: '400px', width: '400px', background: 'var(--bg-secondary)'}}>
+        ←←←←  bg-secondary →→→→
+          <div className='test-sq' style={{height: '300px', width: '300px', background: 'var(--accent-primary)'}}>
+          ←←←  accent-primary →→→
+            <div className='test-sq' style={{height: '200px', width: '200px', background: 'var(--accent-secondary)', color: `var(--bg-primary)`}} >
+            ←←  accent-secondary →→
+              <div className='test-sq' style={{height: '100px', width: '100px', background: 'var(--contrast)', color: `var(--bg-primary)`}}>
+              ←  contrast →
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
