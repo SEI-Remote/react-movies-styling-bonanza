4afbb5952d81de4465583197206fba08f5ca45a8
ManliestBen <benjamintmanley@gmail.com>
2024-05-03 00:37:28 -0500

    add theme selection for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
 import { Route, Routes } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import NavBar from './components/NavBar/NavBar'
 import Landing from './pages/Landing/Landing'
 import MovieList from './pages/MovieList/MovieList'
 import MovieDetails from './pages/MovieDetails/MovieDetails'
 
+import { themeData } from './data/themeData'
+
+
 import './App.css'
 
 function App() {
 
+  const [themeIdx, setThemeIdx] = useState(0)
+
+  useEffect(() => {
+    let newThemeIdx = localStorage.getItem('themeIdx')
+    if (!themeData[newThemeIdx]?.styles) {
+      handleChangeThemeIdx(0)
+    } else {
+      handleChangeThemeIdx(newThemeIdx)
+    }
+  }, [])
+
+  const handleChangeThemeIdx = newIdx => {
+    setThemeIdx(newIdx)
+    localStorage.setItem('themeIdx', newIdx)
+  }
+
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Landing />}/>
-        <Route path='/movies' element={<MovieList />} />
-        <Route path='/movies/:movieId' element={<MovieDetails />} />
-      </Routes>
+      <div style={themeData[themeIdx].styles}>
+        <NavBar 
+          themeIdx={themeIdx} 
+          handleChangeThemeIdx={handleChangeThemeIdx} 
+          themes={themeData}
+        />
+        <Routes>
+          <Route path='/' element={<Landing />}/>
+          <Route path='/movies' element={<MovieList />} />
+          <Route path='/movies/:movieId' element={<MovieDetails />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,20 @@ import { NavLink } from 'react-router-dom'
 import navIcon from '../../assets/icon1.png'
 import './NavBar.css'
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const handleSelectTheme = evt => {
+    props.handleChangeThemeIdx(evt.target.value)
+  }
+
   return (
     <nav>
-      <NavLink to='/'><img src={navIcon} alt="a movie film camera" /></NavLink>
+      <NavLink to='/'><img className='logo' src={navIcon} alt="a movie film camera" /></NavLink>
+      <select name="theme" onChange={handleSelectTheme}>
+        <option value={props.themeIdx}>{props.themes[props.themeIdx].name}</option>
+        {props.themes.map((theme, idx) => 
+          <option key={theme.name} value={idx}>{theme.name}</option>
+        )}
+      </select>
       <NavLink to='/movies'>Movies</NavLink>
     </nav>
   )
